ee11c0d1bb487a0b08b387dccccc33659483ecab
Rajdeepkr1 <76088245+Rajdeepkr1@users.noreply.github.com>
2021-07-19 16:46:11 +0530

    update

--- src/component/gallery/Gallery.jsx
+++ src/component/gallery/Gallery.jsx
@@ -1,71 +1,92 @@
 import "./gallery.scss";
-import Search from '@material-ui/icons/Search';
 import { useState } from "react";
 import { useEffect } from "react";
-import axios from 'axios';
 import Photo from "../details/Photo";
 import InfiniteScroll from 'react-infinite-scroll-component';
 
+
+const clientID = `?client_id=${'fPSfgmvsPYgVYZbryPBLXR-ufETie_SoEa_0EgBUiKk'}`;
+const mainUrl = `https://api.unsplash.com/photos/`;
+const searchUrl = `https://api.unsplash.com/search/photos/`;
+
 const Gallery = () => {
-  const [images, setImage] = useState([]);
-  const [inputSearch, setInputSearch] = useState("");
-  
-  // const callApi = () => {
-  //   fetch(
-  //     "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=33e419272909fde9667e254ea9783126&format=json&nojsoncallback=1&text=cats&extras=url_o"
-  //   )
-  //     .then((response) => response.json())
-  //     .then((data) => setPhoto(data.photos.photo));
-  // };
-  // useEffect(() => {
-  //   callApi();
-  // }, []);
-   
+  const [loading, setLoading] = useState(false);
+  const [photos, setPhotos] = useState([]);
+  const [page, setPage] = useState(0);
+  const [query, setQuery] = useState("");
 
-  
+  const fetchImages = async () => {
+    setLoading(true);
+    let url;
+    const urlPage = `&page=${page}`;
+    const urlQuery = `&query=${query}`
+    if (query) {
+      url = `${searchUrl}${clientID}${urlPage}${urlQuery}`
+    } else {
+      url = `${mainUrl}${clientID}${urlPage}`
+    }
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
 
-  const fetchImages = (count = 100) => {
-    const apiRoot = "https://api.unsplash.com";
-    const accessKey = '3b0NYCsiB-gWCubgOeAtUA73-hNy6Yj_Lejsdufckq4';
-
-    axios
-      .get(`${apiRoot}/photos/random?client_id=${accessKey}&count=${count}`)
-      .then(res => {
-        setImage([...images, ...res.data]);
+      setPhotos((oldPhotos) => {
+        if (query && page === 1) {
+          return data.results;
+        } else if (query) {
+          return [...oldPhotos, ...data.results];
+        } else {
+          return [...oldPhotos, ...data]
+        }
       })
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      setLoading(false)
+    }
   }
+
   useEffect(() => {
-    fetchImages();
-  }, [])
+    
+    fetchImages()
+    // eslint-disable-next-line
+  },[page]);
 
-  function handleSeach(e){
-    e.preventDefault();
-    setInputSearch(e.target.value)
+  useEffect(() => {
+     const event = window.addEventListener("scroll", () => {
+      if ((!loading && window.innerHeight + window.scrollY) >= document.body.scrollHeight - 2) {
+        setPage((oldPage) => {
+          return oldPage + 1;
+        })
+      }
+    })
+    return () => window.removeEventListener("scroll", event)
+    // eslint-disable-next-line
+  },[])
 
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setPage(1)
   }
-
-  
-  
-  console.log(images)
   
   return (
     <div className="gallery">
     <div className="searchBox">
       <h1>Photo gallery</h1>
       <div className="search">
-        <input type="text" placeholder="Search your favourite pictures" value={inputSearch} onChange={handleSeach}/>
-        <Search className="icon"/>
+        <input type="text" placeholder="Search your favourite pictures" value={query} onChange={ (e) => setQuery(e.target.value) }/>
+        
+        <button className="icon"   onClick={ handleSubmit }>Search</button>
       </div>
     </div>
     
     <InfiniteScroll
-        dataLength={images.length}
+        dataLength={photos.length}
         next={fetchImages}
         hasMore={true}
         loader={"Loading..."}
       >
       <div className="picture">
-        {images.map((pic) => {
+        {photos.map((pic) => {
           return (
           <div key={pic.id}>
             <Photo pic={pic}  /> 
